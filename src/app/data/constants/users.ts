ddce8a6445fc5af89d2ef7d2f121d4225c1a9614
Demetrius Carregosa <demetriuscarregosa@gmail.com>
2025-01-24 11:00:53 -0300

    folders and file organization

---
+++ src/app/data/constants/users.ts
@@ -0,0 +1,40 @@
+import { User } from "@/core/model/User";
+
+const users: User[] = [
+    {
+        id: "1",
+        email: "ana@teste.com.br", 
+        name: "Ana",
+        password: "123456",
+    },
+    {
+        id: "2",
+        email: "pedro@teste.com.br", 
+        name: "Pedro",
+        password: "123456",
+    },
+    {
+        id: "3",
+        email: "joao@teste.com.br", 
+        name: "João",
+        password: "123456",
+    },
+    {
+        id: "4",
+        email: "maria@teste.com.br", 
+        name: "Maria",
+        password: "123456",
+    },
+    {
+        id: "5",
+        email: "thiago@teste.com.br", 
+        name: "Thiago",
+        password: "123456",
+    },
+    {
+        id: "6",
+        email: "isabel@teste.com.br", 
+        name: "Isabel",
+        password: "123456",
+    },
+]
