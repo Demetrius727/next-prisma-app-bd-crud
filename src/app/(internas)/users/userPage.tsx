6dddb3f359030062612c0b2b767261a56bbbc218
Demetrius Carregosa <demetriuscarregosa@gmail.com>
2025-01-28 20:48:00 -0300

    create Title component e update userPage

--- src/app/(internas)/users/userPage.tsx
+++ src/app/(internas)/users/userPage.tsx
@@ -1,9 +1,12 @@
 import Page from "@/app/components/template/Page";
+import Title from "@/app/components/template/Title";
 import UserList from "@/app/components/user/UserList";
+import { IconUser } from "@tabler/icons-react";
 
 export default function UserPage() {
     return (
-        <Page>
+        <Page className="flex flex-col gap-10">
+            <Title icon={IconUser} primary="Usuários" secondary="Cadastro de Usuários"/>
             <UserList />
         </Page>
     );
--- src/app/components/template/Page.tsx
+++ src/app/components/template/Page.tsx
@@ -2,13 +2,14 @@ import Menu from "./Menu";
 
 export interface PageProps {
     children: any;
+    className?: string;
 }
 
 export default function Page(props: PageProps) {
     return (
         <div className="flex">
             <Menu />
-            <main className="flex-1 p-7">{props.children}</main>
+            <main className={`flex-1 p-7 ${props.className ?? ''}`}>{props.children}</main>
         </div>
     );
 }
