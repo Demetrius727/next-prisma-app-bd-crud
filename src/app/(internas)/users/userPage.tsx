7df55f96e029b14c68eeb6688e390b5f39de5270
Demetrius Carregosa <demetriuscarregosa@gmail.com>
2025-01-30 21:59:04 -0300

    update userPage e UserForm componentes

--- src/app/(internas)/users/userPage.tsx
+++ src/app/(internas)/users/userPage.tsx
@@ -4,14 +4,22 @@ import Title from "@/app/components/template/Title";
 import UserForm from "@/app/components/user/UserForm";
 import UserList from "@/app/components/user/UserList";
 import users from "@/app/data/constants/users";
+import { User } from "@/core/model/User";
 import { IconUser } from "@tabler/icons-react";
+import { useState } from "react";
 
 export default function UserPage() {
+    const [user, setUser] = useState<User>(users[0]);
+
+    function salvar() {
+        // Salvar no banco de dados
+    }
+
     return (
         <Page className="flex flex-col gap-10">
             <Title icon={IconUser} primary="Usuários" secondary="Cadastro de Usuários"/>
             {/* <UserList /> */}
-            <UserForm user={users[0]}/>
+            <UserForm user={user} onChange={setUser} salvar={salvar} cancelar={() => {}}/>
         </Page>
     );
 }
--- src/app/components/user/UserForm.tsx
+++ src/app/components/user/UserForm.tsx
@@ -3,19 +3,22 @@ import InputText from "../shared/InputText";
 
 export interface UserFormProps {
     user: User  
+    onChange: (user: User) => void
+    salvar: () => void
+    cancelar: () => void   
 }
 
 
 export default function UserForm(props: UserFormProps) {
     return (
         <div className="flex flex-col gap-5">
-            <InputText label="Nome" type="text" value={props.user.name} onChange={() => {}}/>
-            <InputText label="E-mail" type="email" value={props.user.email} onChange={() => {}}/>
-            <InputText label="Senha" type="password" value={props.user.password} onChange={() => {}}/>
+            <InputText label="Nome" type="text" value={props.user.name} onChange={(e) => props.onChange?.({ ...props.user, name: e.target.value })}/>
+            <InputText label="E-mail" type="email" value={props.user.email} onChange={(e) => props.onChange?.({ ...props.user, email: e.target.value })}/>
+            <InputText label="Senha" type="password" value={props.user.password} onChange={(e) => props.onChange?.({ ...props.user, password: e.target.value })}/>
 
             <div className="flex gap-5">
-                <button className="bg-blue-500 px-4 py-2 rounded-md">Salvar</button>
-                <button className="bg-zinc-500 px-4 py-2 rounded-md">Cancelar</button>
+                <button className="bg-blue-500 px-4 py-2 rounded-md" onClick={props.salvar}>Salvar</button>
+                <button className="bg-zinc-500 px-4 py-2 rounded-md" onClick={props.cancelar}>Cancelar</button>
             </div>
         </div>
     )
